31f4d1ac1d8372451f95bd8ad6a71aebb42778b7
Rajasekhar <tatireddyr7887@gmail.com>
2021-12-14 17:39:20 +0530

    second commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from college import views
+
+urlpatterns = [
+    path('',views.home, name="home"),
+    path("stf",views.staff, name="staff"),
+    path('ach', views.Achievements, name="Achievements"),
+    path('port', views.portal, name="portal")
---
+++ myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def staff(request):
+    return render(request, "staff.html")
+
+
+def Achievements(request):
+    return render(request, "achievements.html")
+
+def portal(request):
+    return render(request, "portal.html")
