485f8d011a916b6f37101744a53d6fcbb29bad41
Rajasekhar <tatireddyr7887@gmail.com>
2021-12-14 17:16:22 +0530

    home view

--- college/urls.py
+++ college/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello bye !!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home)
 ]
